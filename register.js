cfc89a5b60edc62933b34d9643a2b3cbdd465802
semakamalapane <malapanesemaka@gmail.com>
2015-03-21 12:52:57 +0000

    Semaka: register user on buzz space

--- register.js
+++ register.js
@@ -1,1 +1,41 @@
+/**
+ * Created by Semaka on 2015/03/21.
+ * Student nr: 13081129
+ */
+//first we're requiring a few dependencies
+var getBuzzSpace = require('createBuzzSpace');
 
+function registerOnBuzzSpaceRequest()
+{
+    var userID = require('login');
+    return userID;
+}
+
+function registerOnBuzzSpace(registerOnBuzzSpaceRequest)
+{
+    if(!getBuzzSpace.isOpen)
+        return new Error("Buzz space not active");
+    else if(registerOnBuzzSpaceRequest)
+        return new Error("User not authorized to use this buzz space");
+    else if(getBuzzSpace.isOpen && getProfileForUser(registerOnBuzzSpaceRequest)!= "No such user")
+        return getProfileForUser(registerOnBuzzSpaceRequest);
+    else
+        return new ProfileData();
+}
+
+//ProfileData constructor
+function ProfileData()
+{
+    var userNameForBuzzSpace;
+    var photo;
+    var signature;
+}
+
+//gets the user profile
+function getProfileForUser(user)
+{
+    if(user)
+        return user.ProfileData();
+    else
+        return new Error("No such user");
+}
