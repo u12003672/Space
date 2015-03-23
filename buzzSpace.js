93a68a249fa670dae29d138e51742e6fb4330f0b
TsepoNtsaba <therealmrntsaba@gmail.com>
2015-03-23 18:33:47 +0200

    Create the JSDocs comment on the buzzSpace class just need to generate so that it actually creates an html

--- buzzSpace.js
+++ buzzSpace.js
@@ -21,23 +21,23 @@ BuzzSpace = {
     academicYear: "",
     isActive: "",
 	
-    /*create buzz sapce request which takes in the users login details and moduleID and creates a login request to recieve a userID*/			
+    /** create buzz sapce request which takes in the users login details and moduleID and creates a login request to recieve a userID @constructor */			
     createBuzzSpaceRequest: function(_username, _password, _moduleID)
     {
-        this.userID = loginRequest(_username, _password);//First see if the user is logged in, if not log them in
+        this.userID = loginRequest(_username, _password);/**First see if the user is logged in, if not log them in */
         this.moduleID = _moduleID;
 	var today = new Date();    
         this.academicYear = today.getFullYear();
 	
-	if(this.userID != null && this.moduleID)//If the user exists and the module exists
+	if(this.userID != null && this.moduleID)/**If the user exists and the module exists*/
 	{	    
-		this.createBuzzSpace(this.userID, this.moduleID, this.academicYear);//Call the createBuzzSpace function with the user, module and year as parameters 
+		this.createBuzzSpace(this.userID, this.moduleID, this.academicYear);/**Call the createBuzzSpace function with the user, module and year as parameters*/
 	}
 
     },
 
 
-    /*Critical main function that checks for all the preconditions for the BuzzSpace to be created.
+    /**Critical main function that checks for all the preconditions for the BuzzSpace to be created.
     *It then returns a service by creating a BuzzSpace and that a root thread has been created for BuzzSpace*/
     createBuzzSpace: function (_userID, _moduleID, _academicYear) {
 
@@ -49,10 +49,10 @@ BuzzSpace = {
                 {
                     if(this.getUserRoleForModule(_userID) == true)
                     {
-                        this.storeBuzzSpace();//Stores the space to a mongoDB or persitant entityManager
-                        this.registerOnSpace(_userID);//register the lecturer on the space
-                        this.assignAdministrator(_userID);//Assign the lecture as an admin on the space
-                        this.submitPost();//Create a root thread and a nice welcome message
+                        this.storeBuzzSpace();/** Stores the space to a mongoDB or persitant entityManager*/
+                        this.registerOnSpace(_userID);/**register the lecturer on the space*/
+                        this.assignAdministrator(_userID);/**Assign the lecture as an admin on the space*/
+                        this.submitPost();/**Create a root thread and a nice welcome message*/
                     }
                     else{
                         throw "The user is not authorised to create a BuzzSpace";
@@ -72,20 +72,20 @@ BuzzSpace = {
     },
 
 
-    /*closes a buzz space and renders it inactive*/
+    /** closes a buzz space and renders it inactive*/
     closeBuzzSpace: function()
     {
             this.isOpen = false;
     },
 
-    /*Provides a persistent services provided by a JPA-compliant entitymManager to select the BuzzSpace*/
+    /** Provides a persistent services provided by a JPA-compliant entitymManager to select the BuzzSpace*/
     getBuzzSpaceRequest: function(_moduleID)
     {
-	//While it queries if finds a sapceID for the module return the spaceID or null
+	/** While it queries if finds a sapceID for the module return the spaceID or null*/
         return _spaceID;
     },
 
-    /*returns true or false based on if the space exists or not*/
+    /** returns true or false based on if the space exists or not*/
     getBuzzSpace: function (_moduleID) {
 
         if(getBuzzSpaceRequest(_moduleID) != null)
@@ -101,28 +101,28 @@ BuzzSpace = {
 
     
 
-    //Gets all the active modules from CSDS Adapter for that specific year and semester
+    /**Gets all the active modules from CSDS Adapter for that specific year and semester*/
     getActiveModulesForYear: function (_year_group) {
 
         if(getActiveModulesForYearRequest(_year_group) == "Lecture")
-            return false; //If the Module is not active for that particular year and semester return false
+            return false; /**If the Module is not active for that particular year and semester return false*/
         else
-            return true;//else-if the Module is active for that year return true
+            return true;/** else the Module is active for that year return true*/
 
 
     },
 
-    /*Connects with the CSDataSources adapter to get the user roles for the module, this function is implemented by the buzzDataSource team e*/
+    /** Connects with the CSDataSources adapter to get the user roles for the module, this function is implemented by the buzzDataSource team */
     getUsersRolesForModule: function (_userId) {
 
-        /*if(_userId == "Lecturer")
+        /**if(_userId == "Lecturer")
             return false; //check to see if the passed user Id is a lecturer, if it is not return false
         else
             return true;//if the passed user is a lecture return true*/
 
     },
 
-    /*Provides a persistent services provided by a JPA-compliant entitymManager to store the BuzzSpace*/
+    /**Provides a persistent services provided by a JPA-compliant entitymManager to store the BuzzSpace*/
     storeBuzzSpace: function () {
 
         var databaseUrl = "db";
@@ -139,17 +139,17 @@ BuzzSpace = {
 
     },
 
-    /*Creates a profile on buzz space for that user*/
+    /**Creates a profile on buzz space for that user*/
     registerOnSpace: function (_userId) {
         /*register lecturer on BuzzSpace*/
     },
 
-    /*The lecurer is assigned to the administrator of the space can later assign other administrators*/
+    /**The lecurer is assigned to the administrator of the space can later assign other administrators*/
     assignAdministrator: function (_userId) {
 
     },
 
-    /*Create the root thread for BuzzSpace*/
+    /**Create the root thread for BuzzSpace*/
     submitPost: function (_moduleID) {
 
     },
