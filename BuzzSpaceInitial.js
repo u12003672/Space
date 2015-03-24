386960e31b126d80322231437443867c4edd4e01
GodfreyMathe13103394 <u13103394@tuks.co.za>
2015-03-23 22:51:17 -0300

    Re implemented some functions  + JavaDocs
    Re implemented some functions and added comments (javaDoc) style

--- BuzzSpaceInitial.js
+++ BuzzSpaceInitial.js
@@ -11,6 +11,11 @@ BuzzSpace = {
     isOpen: "",
     academicYear: "",
     activeSpaces:"",
+    _userValid:"",
+    _userRoleValid:"",
+    _spaceExists:"",
+    admin:"",
+    users:"",
 
 
     /**
@@ -47,49 +52,49 @@ BuzzSpace = {
         /**
          * Make the user an admin for the space created
          */
-            createAdministrator(_userID);
+            assignAdministrator(_userID);
 
         /**
          * Store space to persistance
          */
-            storeSpace();
+            storeBuzzSpace();
     },
 
 
+    /**
+     *
+     * @BuzzSpaceExistsException
+     *
+     * Throw this exception when a buzz space already exists with same module id
+     */
     BuzzSpaceExistsException: function () {
 
         print("Buzz Space Already Exists");
     },
 
 
-    registerOnSpace: function () {
+    /**
+     * Function that creates a request
+     * and checks if the user is authorised to create a space and wether a space for the moduleID does not exist yet
+     * @param _userID stores the ID of the user
+     * @param _moduleID represents the module code
+     */
+
+    createBuzzSpaceRequest: function (_userID,_moduleID) {
+
+        this.userValid = validateUser(_userID); //return true if the user is recognised , Done by authorization through inteceptors
+        this.userRoleValid = userRoleValid(_userID); // check to return true see if the user is a lecturer else return null
+        this.spaceExists = spaceExists(_moduleID); //return true if space exists else null
 
     },
 
 
-    createRootThread: function () {
-
-    },
-
-    createBuzzSpaceRequest: function () {
-
-        userRole = getUsersRolesForModule(_userId);
-        try {
-                if(userRole != "Lecturer")
-                    throw "You are not authorized to create a BuzzSpace";
-
-        }
-        catch(err)
-        {
-            print(err.message);
-        }
-
-        storeBuzzSpace(_moduleID, _isOpen ,_academicYear);
-
-
-    },
-
-
+    /**
+     * This function simply stores the created space to the database
+     * @param _moduleID represents the module code
+     * @param _isOpen boolean to check if space is Active
+     * @param _academicYear the year for which the space is to be created
+     */
     storeBuzzSpace: function (_moduleID, _isOpen ,_academicYear) {
 
         var databaseUrl = "db";
@@ -104,82 +109,98 @@ BuzzSpace = {
 
     },
 
-    getActiveModules: function () {
+    /**
+     * Authorization module intercepts with this function to validate a user
+     * @param _userID stores the user ID
+     */
+    validateUser: function (_userID) {
 
-        // get modules from CSDS ADApter
-
-        activeModule = getActiveModule();
-
-        try {
-            while (activeModules.hasNext()) {
-                if (activeModules.moduleID == moduleID) {
-                    if(activeModules.isActive() == false)
-                        throw "The module you are trying to access is not active.";
-                }
-            }
-        }
-        catch(err)
-        {
-            print(err.message);
-        }
-
-        return true;
+    //Authorization module to be included to allow for this method
 
     },
 
-    getUsersRolesForModule: function () {
+    /**
+     *Authorization module intercepts with this function to validate a users role
+     * @param _userID stores the user ID
+     */
+
+    userRoleValid: function (_userID) {
 
         // get user roles from CSDS Adapter
+        // Authorization module to be included to allow for this method
 
     },
 
-    assignAdministrator: function () {
 
+    spaceExists: function (_moduleID) {
 
+        // get active spaces from MongoDB
+        // Authorization module to be included to allow for this method
 
     },
 
-    getBuzzSpace: function (_moduleID) {
+    assignAdministrator: function (_userID) {
 
-        activeSpaces = getActiveSpaces();
-        try {
-            while (activeSpaces.hasNext()) {
-                if (activeSpaces.moduleID == moduleID) {
-                    throw "Buzz Space Already Exists";
-                }
-            }
-        }
-        catch(err)
-        {
-            print(err.message);
-        }
+        this.admin = _userID;
 
-        return false;
     },
 
 
-
-
+    /**
+     * getter function for moduleID
+     * @returns {string} returns the module ID
+     */
     getModuleId: function(){
         return this.moduleID;
     } ,
 
+
+    /**
+     * getter function for the academic year
+     * @returns {string} returns the module ID
+     */
     getAcademicYear: function(){
         return this.academicYear;
     } ,
 
+    /**
+     * function returns true if space is active
+     * @returns {boolean} returns true or false
+     */
     isActive: function(){
 
         return this.isOpen;
 
     },
 
-    deleteBuzzSpace: function () {
+    /**
+     * This function deletes the space from MongoDb
+     * @param _moduleID represents the module code
+     */
 
-        deleteMongoPersistence();
+    deleteBuzzSpace: function (_moduleID) {
+
+        deleteFromDb(_moduleID);
+
+    },
 
 
-    }
+    /**
+     *Function simply adds the new user to a list of registered users
+     * @param userID represents the user ID
+     * @param _moduleID represents the module code
+     */
+    registerOnSpace: function (userID,_moduleID) {
+
+            users.add(userID,_moduleID);
+    },
+
+    /**
+     * Threads module to be required for this method hat creates a root thread for the space
+     */
+    createRootThread: function () {
+
+    },
 
 
 };
